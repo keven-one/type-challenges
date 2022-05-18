211342d76e0d7b62c709a1499a23faa31e92c1f2
Fate <574392788@qq.com>
2022-05-18 13:40:28 +0800

    ts-challenage

---
+++ challenges/00898-easy-includes/template.ts
@@ -0,0 +1,3 @@
+import { Equal } from "@type-challenges/utils";
+
+export type Includes<T extends readonly any[], U> = T extends [infer First,... infer Rest] ? Equal<First,U> extends true ? true : Includes<Rest,U> : false; 
