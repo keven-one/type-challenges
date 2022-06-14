609b98ae5d383b9efdb2e255236ed26f3234ec5a
Fate <yongchao.wang@ingka.com>
2022-06-14 17:52:46 +0800

    ts-challenage

---
+++ challenges/medium/02257-medium-minusone/template.ts
@@ -0,0 +1,4 @@
+type MinusOne<T extends number, A extends any[] = []> =
+  [...A, any, any]['length'] extends T ? [...A, any]['length'] :
+  [...A, any]['length'] extends T ? A['length'] :
+  MinusOne<T, [...A, any, any]>
